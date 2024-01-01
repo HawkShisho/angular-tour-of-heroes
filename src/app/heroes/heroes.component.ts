d119212a434adb0aad6060a1ab808a1eaf2abb7e
hawkshisho <nil.lazo@alumnos.duoc.cl>
2023-12-31 21:22:00 -0300

    app update

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
+
+@Component({
+  standalone: true,
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+  imports: [
+    FormsModule,
+    NgIf,
+    NgFor,
+    UpperCasePipe,
+    HeroDetailComponent,
+  ],
+
+})
+export class HeroesComponent {
+
+  heroes : Hero[] = [];
+
+  selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void{
+    this.heroes = this.heroService.getHeroes()
+  }
+
+}
